b5e9354fcd6e1d957685a172fdf9be8868f6a40a
D.B.S.Dewlini <sehansadewlini27@gmail.com>
2026-03-14 22:33:25 +0530

    Add java conditional programming programs

---
+++ java_conditions.java
@@ -0,0 +1,26 @@
+// Higher diploma in Information Technology - Year one Java programming
+// Write a program to get 2 values as command line arguments and find the Minimum of those 2 using if-else.
+
+class java_conditions{
+    public static void main(String[] args){
+        if(args.length<2){
+            System.out.println("Please enter two values");
+            return;
+        }
+
+        int num1=Integer.parseInt(args[0]);
+        int num2=Integer.parseInt(args[1]);
+
+        int minimumNumber;
+
+        if(num1<num2){
+            minimumNumber=num1;
+        }
+        else{
+            minimumNumber=num2;
+        }
+
+        System.out.println("The minimum number is: "+minimumNumber);
+
+    }
+}
